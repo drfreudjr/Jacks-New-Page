6dff59e0ec2eab53c0932eaee16bffb86aefd2a3
DrFreudJr <drfreudj88@gmail.com>
2021-02-16 18:12:27 -0500

    Initial commmit

---
+++ js/shell.js
@@ -0,0 +1,10 @@
+/* Javascript Shell */
+// 'use strict';
+let cl = console.log;
+
+document.write("Javascript Active: " + Date());
+
+$(document).ready(function(){
+    $('div').html('jQuery online')
+
+}); 
