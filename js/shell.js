95d2dfad37704bbb28066230aab9fdbd30748f5e
DrFreudJr <drfreudj88@gmail.com>
2021-02-16 22:33:12 -0500

    refactor

--- js/shell.js
+++ js/shell.js
@@ -3,18 +3,17 @@ import { fontList } from './modules/fontList.js';
 
 const global = {
     name : 'Jack Wilcox',
-    eachNameLetter : []
+    eachLetter : [] // eventually create array of objects for each letter
 }
 
 main()
 function main() {
-    makeObjectArrayForEachLetter ()
+    makeEachLetterAnObjectAndPutItIntoAnArray ()
 }
 
-function makeObjectArrayForEachLetter () {
+function makeEachLetterAnObjectAndPutItIntoAnArray  () {
     for (let i= 0; i < global.name.length; i++) 
-        global.eachNameLetter[i] = {'letter': global.name[i]}
+        global.eachLetter[i] = {'letter': global.name[i]}
 }
 
-
-cl(global.eachNameLetter[0].letter)
+cl(global.eachLetter[0].letter)
