c416b1a0eacdb127acc40908a3430e08e6a7181b
DrFreudJr <drfreudj88@gmail.com>
2021-02-17 18:39:32 -0500

    added create random string

--- js/shell.js
+++ js/shell.js
@@ -4,20 +4,30 @@ import { dynamicFontSize } from './modules/dynamicFontSize.js';
 
 const global = {
     name : 'Jack Wilcox',
-    eachLetter : [] // eventually create array of objects for each letter
+    fontSize : 14,
+    eachLetter : [{}], // create array of objects for each letter
 }
 
 main()
 function main() {
-    makeEachLetterAnObject ()
+    makeEachLetterAnObject()
+    // printNameAnimation()
 }
 
-function makeEachLetterAnObject () {
+function makeEachLetterAnObject() {
     for (let i= 0; i < global.name.length; i++) 
-        global.eachLetter[i] = {'letter': global.name[i]}
+        global.eachLetter[i] = {'letter': global.name[i]} 
 }
 
-cl(global.eachLetter[1].letter)
+getRandomCharacterString()
 
-let test = dynamicFontSize(14,1360)
-cl(test)
+function getRandomCharacterString () {
+    let possibleLetters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    let randomString = ''
+    let randomCharacter = null
+    for (let i = 0; i < global.name.length; i++) {
+        randomCharacter = possibleLetters[Math.floor(Math.random()*62)]
+        randomString += (randomCharacter)
+    }
+}
+
