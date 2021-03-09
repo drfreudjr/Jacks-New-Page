d0413b994f0f244bfd8671921e7401d618cc9fb9
DrFreudJr <drfreudj88@gmail.com>
2021-03-08 21:43:04 -0500

    nesting

--- js/shellCanvas.js
+++ js/shellCanvas.js
@@ -55,7 +55,7 @@ var context;   // Global canvas object reference
 
 function main() {  // wrapper that gets called on resize event
 
-const boxSize = flickerAnim.widthPercentage*innerWidth/flickerAnim.phraseToDraw.length
+const boxSize = flickerAnim.widthPercentage*innerWidth/flickerAnim.phraseToDraw.length  // dynamic resize parameters
 const letterSize = flickerAnim.letterToBoxRatio*boxSize // calculated initial values
 const charcatersToLockIn = flickerAnim.phraseToDraw.length
 let charactersLockedIn = 0
@@ -72,6 +72,50 @@ for (let i = 0; i < flickerAnim.phraseToDraw.length; ++i) {
 
 lettersAnimation()
 function lettersAnimation () {
+    function drawLetter () { 
+        function drawChosenLetter (positionToChange, letterToInsert) {
+
+            function getCenterXPosition (randomCharacter, stringPlacementX, boxSize) {  // returns x position to draw letter in box
+               let textWidth = context.measureText(randomCharacter).width
+               let centerOfBox = stringPlacementX + (.5*boxSize)
+               let xPosition = centerOfBox - (.5*textWidth)
+              return (xPosition)
+           }  
+
+          let stringPlacementX = startingArraySpotX+((positionToChange-1)*boxSize) // set box position
+          let stringPlacementY = innerHeight*flickerAnim.verticalPlacementPercentage
+
+          context.fillStyle = flickerAnim.darkColor  // background box color
+              // Y vertical made bigger to erase partular letters that exceed box size
+         context.fillRect(stringPlacementX, stringPlacementY*.5, boxSize, boxSize*10) // erases previous letter
+                                                                    
+
+          let xPosition = getCenterXPosition(letterToInsert, stringPlacementX, boxSize) // position letter in square
+          let yPosition = stringPlacementY + (.77*boxSize) // hacky center letter vertically
+
+          context.fillStyle = flickerAnim.medColor
+           context.fillText (letterToInsert, xPosition, yPosition) // draw the damn thing
+       }
+
+       flickerAnim.totalNumberofPaints ++   // simple overall counter
+       for (let i = 0; i < cyclesPerFrame; ++i) { // letters to change per paint
+            let positionToChange = (Math.floor(Math.random()*flickerAnim.phraseToDraw.length)) //default random
+            let letterToInsert = randomCharacterString(1) // external module call <arg> is length                
+            if (flickerAnim.totalNumberofPaints >= flickerAnim.numberPaintsBeforeNextLock) {// time to a lock letter!
+                flickerAnim.numberPaintsBeforeNextLock += Math.floor(Math.random()*flickerAnim.maximumDelayBetweenLetterLocks + 1) // increment next time to lock
+                charactersLockedIn ++  
+               let j = Math.floor(Math.random()*flickerAnim.randomNoReplacementArray.length)
+                  positionToChange = flickerAnim.randomNoReplacementArray[j]
+                flickerAnim.randomNoReplacementArray.splice(j,1)   // remove the chosen array member
+                  flickerAnim.letterLockedIn[positionToChange] = true // indicate locked in
+        }
+          if (flickerAnim.letterLockedIn[positionToChange] == true)
+               letterToInsert = flickerAnim.phraseToDraw[positionToChange] // swap in locked letter
+           drawChosenLetter(positionToChange, letterToInsert)
+        }   
+} 
+
+
     if (charactersLockedIn < charcatersToLockIn) {
         if (fps > flickerAnim.cyclesBeforeOverdrive) { 
             ++ cyclesPerFrame // draw multiple cycles per render
@@ -85,49 +129,6 @@ function lettersAnimation () {
         }, 1000 / fps)
     }
 }
-
-function drawLetter () { 
-    function drawChosenLetter (positionToChange, letterToInsert) {
-
-        function getCenterXPosition (randomCharacter, stringPlacementX, boxSize) {  // returns x position to draw letter in box
-           let textWidth = context.measureText(randomCharacter).width
-           let centerOfBox = stringPlacementX + (.5*boxSize)
-           let xPosition = centerOfBox - (.5*textWidth)
-           return (xPosition)
-        }  
-
-       let stringPlacementX = startingArraySpotX+((positionToChange-1)*boxSize) // set box position
-       let stringPlacementY = innerHeight*flickerAnim.verticalPlacementPercentage
-
-       context.fillStyle = flickerAnim.darkColor  // background box color
-           // Y vertical made bigger to erase partular letters that exceed box size
-       context.fillRect(stringPlacementX, stringPlacementY*.5, boxSize, boxSize*10) // erases previous letter
-                                                                    
-
-        let xPosition = getCenterXPosition(letterToInsert, stringPlacementX, boxSize) // position letter in square
-        let yPosition = stringPlacementY + (.77*boxSize) // hacky center letter vertically
-
-        context.fillStyle = flickerAnim.medColor
-        context.fillText (letterToInsert, xPosition, yPosition) // draw the damn thing
-    }
-    
-    flickerAnim.totalNumberofPaints ++   // simple overall counter
-    for (let i = 0; i < cyclesPerFrame; ++i) { // letters to change per paint
-        let positionToChange = (Math.floor(Math.random()*flickerAnim.phraseToDraw.length)) //default random
-        let letterToInsert = randomCharacterString(1) // external module call <arg> is length                
-        if (flickerAnim.totalNumberofPaints >= flickerAnim.numberPaintsBeforeNextLock) {// time to a lock letter!
-            flickerAnim.numberPaintsBeforeNextLock += Math.floor(Math.random()*flickerAnim.maximumDelayBetweenLetterLocks + 1) // increment next time to lock
-            charactersLockedIn ++  
-            let j = Math.floor(Math.random()*flickerAnim.randomNoReplacementArray.length)
-            positionToChange = flickerAnim.randomNoReplacementArray[j]
-            flickerAnim.randomNoReplacementArray.splice(j,1)   // remove the chosen array member
-            flickerAnim.letterLockedIn[positionToChange] = true // indicate locked in
-        }
-        if (flickerAnim.letterLockedIn[positionToChange] == true)
-            letterToInsert = flickerAnim.phraseToDraw[positionToChange] // swap in locked letter
-        drawChosenLetter(positionToChange, letterToInsert)
-    }   
-} 
 
 
 
