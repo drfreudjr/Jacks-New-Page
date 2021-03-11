b24a8961d91911dcc535b720eb3a160cae573e56
DrFreudJr <drfreudj88@gmail.com>
2021-03-10 19:56:32 -0500

    moved evrything into a function

--- js/modules/flicker.js
+++ js/modules/flicker.js
@@ -1,12 +1,4 @@
-const cl = console.log;
-// import { fontList } from './modules/fontList.js';
-// import { dynamicFontSize } from './modules/dynamicFontSize.js';
-import { randomCharacterString } from './modules/randomCharacterString.js' // arg = length
-
-textAnim()
-function textAnim() {
-
-window.onload = function () {           // onload wrapper
+export function flicker() {
 
 let flickerAnim = {   // flickerAnim global object
     phraseToDraw : 'Jack Wilcox Productions',
@@ -133,9 +125,4 @@ function lettersAnimation () {
     }
 }
 
-
-
-}   // end drawScreen wrapper
-}   // end onload wrapper
-
-}   // end textAnim
+} // flicker
