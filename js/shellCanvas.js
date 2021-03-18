b188092b1b13554446ebfb9d42547485d0f57b77
DrFreudJr <drfreudj88@gmail.com>
2021-03-18 18:38:28 -0400

    rf

--- js/shellCanvas.js
+++ js/shellCanvas.js
@@ -49,8 +49,8 @@ for (let i = 0; i < flickerAnim.phraseToDraw.length; ++i) {
     flickerAnim.randomNoReplacementArray[i] = i
 }
 
-function getCenterXPosition (randomCharacter, stringPlacementX, boxSize) {  // returns x position to draw letter in box
-    let textWidth = context.measureText(randomCharacter).width
+function getCenterXPosition (letterToInsert, stringPlacementX, boxSize) {  // returns x position to draw letter in box
+    let textWidth = context.measureText(letterToInsert).width
     let centerOfBox = stringPlacementX + (.5*boxSize)
     let xPosition = centerOfBox - (.5*textWidth)
     return (xPosition)
